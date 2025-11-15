d6245dee059cfa2da6afcfd3a331b87b3b9324a6
naomiacontreras <naomi.miranda.contreras@gmail.com>
2025-11-14 23:34:14 -0600

    Create Ejercicio3.c

---
+++ Ejercicio3.c
@@ -0,0 +1,14 @@
+/*Miranda Contreras Naomi Alexa Tarea 2*/
+
+#include <stdio.h>
+
+int main() 
+{
+    float dolares, pesos;
+    printf("Ingrese la cantidad en dolares: ");
+    scanf("%f", &dolares);
+    pesos=dolares*11.96;
+    printf("Equivalente en pesos: %f\n", pesos);
+
+    return 0;
+}
